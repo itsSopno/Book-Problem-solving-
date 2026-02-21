64a60f73563e47b88144d9047fc3a2114d519205
unknown <>
2026-02-21 10:48:12 +0600

    save changes

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 // chapter of logic 
 // problem number 1 
-function avrage(numbers){
-    const even = [];
-    for(const number of numbers){
-        if(number % 2 === 0 ){
-            even.push(number);
-        }
-    }
-   let sum = 0 ;
-   for(const evenNumber of even ){
-    sum += evenNumber;   }
-    const count = even.length ;
-    // const avarage = sum / count ;
-    return count;
-}
+// function avrage(numbers){
+//     const even = [];
+//     for(const number of numbers){
+//         if(number % 2 === 0 ){
+//             even.push(number);
+//         }
+//     }
+//    let sum = 0 ;
+//    for(const evenNumber of even ){
+//     sum += evenNumber;   }
+//     const count = even.length ;
+//     // const avarage = sum / count ;
+//     return count;
+// }
 
-const numbers = [1 , 2, 3, 4, 5, 6,7,8,9,10];
-const result = avrage(numbers);
-console.log(result)
+// const numbers = [1 , 2, 3, 4, 5, 6,7,8,9,10];
+// const result = avrage(numbers);
+// console.log(result)
 
 // problem number 2 
 
-function multiply (number){
-    const odds = [];
-    for(const num of number ){
-        if(num % 2 === 1 ){
-            const result = num *3 ;
-            odds.push(result);
+// function multiply (number){
+//     const odds = [];
+//     for(const num of number ){
+//         if(num % 2 === 1 ){
+//             const result = num *3 ;
+//             odds.push(result);
         
+//         }
+//     }
+//     return odds;
+// }  
+
+// const number = [1 , 2, 3, 4, 5, 6,7,8,9,10];
+// const result2 = multiply(number);
+// console.log(result2);
+
+// problem number 3 
+function foundOdd (number){
+    const odd = [];
+    for(const num of number){
+        if(num % 2 === 1 ){
+                odd.push(num)
+
         }
     }
-    return odds;
-}  
+    if(odd.length === 0 ){
+        return "there is no odd number"
+    }
+    return odd
+}
 
-const number = [1 , 2, 3, 4, 5, 6,7,8,9,10];
-const result2 = multiply(number);
-console.log(result2);
+const number = [2,4 ,5,6,7,8,13]
+const result3 = foundOdd(number)
+console.log(result3)
