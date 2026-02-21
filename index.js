bd825f456ea6baca147d8dec4cfe5825d1499fce
unknown <>
2026-02-21 10:36:40 +0600

    save push

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ const numbers = [1 , 2, 3, 4, 5, 6,7,8,9,10];
 const result = avrage(numbers);
 console.log(result)
 
+// problem number 2 
+
+function multiply (number){
+    const odds = [];
+    for(const num of number ){
+        if(num % 2 === 1 ){
+            const result = num *3 ;
+            odds.push(result);
+        
+        }
+    }
+    return odds;
+}  
+
+const number = [1 , 2, 3, 4, 5, 6,7,8,9,10];
+const result2 = multiply(number);
+console.log(result2);
